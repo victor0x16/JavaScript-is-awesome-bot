c3091dc5f385c984cabcd74383cc4e0a1d2846d1
Victor Souza <taffavitoa710@gmail.com>
2022-03-30 00:39:29 -0300

    remove app.js

--- api/index-green.js
+++ api/index-green.js
@@ -5,14 +5,12 @@ const axios = require("axios");
 
 
 const PORT = process.env.PORT || 5000;
-const BEARER_TOKEN = process.env.BEARER_TOKEN;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const TWITTER_API = process.env.TWITTER_API || "https://api.twitter.com/2/";
+const TWITTER_API = process.env.TWITTER_API;
 const CODE_VERIFIER = process.env.CODE_VERIFIER;
-
-console.log(process.env)
+const CALLBACK = process.env.CALLBACK;
 
 function postTweet(client_response) {
 	axios({
@@ -41,12 +39,31 @@ function authorizationCode(client_response, code) {
 	  data: { 
 	  	code:code,
 	  	grant_type:"authorization_code",
-	  	redirect_uri:"https://dev3000a7b.herokuapp.com/callback",
-	  	code_verifier: "kff0ffuffaffuffaffiffiffafflffaffoffo"
+	  	redirect_uri: CALLBACK + ,
+	  	code_verifier: CODE_VERIFIER
 	  },
 	  headers: {
 	   "accept":"application/json",
 	   "authorization": `Basic ${btoa(CLIENT_ID + ":" + CLIENT_SECRET)}`,
+	   "content-type": "application/json"
+	  }
+	}).then(response => {
+		client_response.send(JSON.stringify(response.data));
+	}).catch(err => {
+		client_response.send(err.message);
+	});
+}
+
+
+function getTwitter(client_response, id) {
+	axios({
+	  method: "get",
+	  url: TWITTER_API + "tweets",
+	  params: {
+	  	ids: id || 20
+	  },
+	  headers: {
+	   "authorization": `Bearer ${ACCESS_TOKEN}`,
 	   "content-type": "application/json"
 	  }
 	}).then(response => {
@@ -66,29 +83,12 @@ app.get("/callback", function (request, response) {
 	authorizationCode(response, code);
 });
 
-app.get("/test" , function (request, client_response) {
-	axios({
-	  method: "get",
-	  url: TWITTER_API + "tweets",
-	  params: {
-	  	ids: 20
-	  },
-	  headers: {
-	   "authorization": `Bearer ${ACCESS_TOKEN}`,
-	   "content-type": "application/json"
-	  }
-	}).then(response => {
-		client_response.send(JSON.stringify(response.data));
-	}).catch(err => {
-		client_response.send(err.message);
-	});
+app.get("/tweet" , function (request, response) {
+	getTwitter(response);
 });
 
-app.get("/test2" , function (request, response) {
-	postTweet(response);
-	
-})
 
 app.listen(PORT, function () {
 	console.log(PORT);
 });
+
